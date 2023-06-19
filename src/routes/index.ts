8f02b913bbcc768eb33be1a0eda3576035dc5a47
superdev07197 <origindev0719@gmail.com>
2023-06-19 12:19:46 +0300

    add test endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,5 +10,8 @@ router.post("/personality", PersonalityController.getPersonality);
 router.post("/result", PersonalityController.result);
 router.post("/login", UserController.signin);
 router.post("/signup", UserController.signup);
+router.get("/test", (req, res) => {
+  res.json("server is working fine");
+});
 
 export default router;
